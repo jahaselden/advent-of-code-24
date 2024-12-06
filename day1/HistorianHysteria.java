540dc2603c24ff8d70da5cc6f8e6f76294e575d1
Jenny Haselden <jennyhaselden7@gmail.com>
2024-12-06 10:13:03 -0800

    start of day 1

---
+++ day1/HistorianHysteria.java
@@ -0,0 +1,10 @@
+import java.io.File; 
+import java.io.FileNotFoundException; 
+import java.util.Scanner;
+
+public class HistorianHysteria {
+    public static void main(String[] args) {
+
+    }
+
+}
